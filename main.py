23e5b0b5aa4550fee2bf7ac430c3ccb73555e3e1
Mohamed Amine Taif <99884252+TaifMohamedAmine@users.noreply.github.com>
2023-05-23 09:55:28 +0100

    Add files via upload

--- main.py
+++ main.py
@@ -5,22 +5,35 @@ import torch.optim as optim
 import random 
 from Preprocess import Prepare_data
 import time
+from sklearn.preprocessing import StandardScaler
 
 
 class Model(nn.Module) :
     '''
     Our model training class :p
     '''
-    def __init__(self, batches, char_idx, embedding_size, hidden_size, num_layers, teacher_forcing_ratio, dropout = 0.2, learning_rate = 0.001, training_ratio = 0.8):
+    def __init__(self, data, char_idx, batch_size,  embedding_size, hidden_size, num_layers, teacher_forcing_ratio, dropout = 0.2, learning_rate = 0.001, training_ratio = 0.8):
         super().__init__()
         '''
         first we define our models parameters
         '''
+        # our data delimiters :
+        self.sow = '$'
+        self.eow = '£'
+
+        # beam search decoder global list and counter 
+        self.counter = 0
+        self.result_list = []
+
         # our optimizer learning rate :
         self.lr = learning_rate
 
-        # our training batches : 
-        self.batches = batches
+        # our training data : 
+        self.data = data
+
+        # our batches :
+        self.batches = None
+        self.batch_size = batch_size 
 
         # our training ratio : 
         self.ratio = 0.8
@@ -36,6 +49,9 @@ class Model(nn.Module) :
 
         # the dropout ratio for our layers : 
         self.dropout = dropout
+
+        # our training and validation batches 
+        self.train_batches, self.val_batches = self.prepare_batches()
 
         '''
         Now Let's define our Neural Network structure 
@@ -58,7 +74,7 @@ class Model(nn.Module) :
         # the encoder layer
         self.BILSTM = nn.LSTM(input_size=self.embedding_size, hidden_size=self.hidden_size, num_layers=self.num_layers, bidirectional=True, batch_first=True, dropout = self.dropout)
         self.input_dense = nn.Linear(self.hidden_size * 2,self.embedding_size)
-        
+
         # the decoder layer
         self.LSTM = nn.LSTM(input_size= self.embedding_size ,hidden_size = self.hidden_size*2, num_layers = self.num_layers, batch_first = True , dropout = self.dropout)
 
@@ -70,7 +86,7 @@ class Model(nn.Module) :
 
         # Our optimizers used : 
         self.opt1 = optim.Adam(self.BILSTM.parameters(), lr = self.lr )
-        self.opt2 = optim.Adam([*self.LSTM.parameters(), *self.input_dense.parameters()], lr = self.lr)
+        self.opt2 = optim.Adam([*self.LSTM.parameters(), *self.input_dense.parameters(), *self.Linear.parameters()], lr = self.lr)
 
         print("all the parameters are initialized")
 
@@ -160,16 +176,14 @@ class Model(nn.Module) :
             Dense_decoded_output = self.Linear(decoder_output)
             soft = nn.Softmax(dim = 2)
             soft_out = soft(Dense_decoded_output)
-            #tst = torch.squeeze(soft_out, 1)
-            #[128, 39]
-            #tmp = torch.topk(tst, 3, dim = 1).tolist()
-            #topk_indexes.append(tmp)
             outputs.append(soft_out)
         return outputs
     
 
     def train_model(self, batches, teacher_forcing_bool, epoch):
-                
+        '''
+        training function 
+        '''
         train_batches = batches           
         epoch_loss = 0
         n = 0            
@@ -211,7 +225,7 @@ class Model(nn.Module) :
         with torch.no_grad() :
             for batch in val_batches :
                 root_batch, encoder_output ,encoder_states = self.encode(batch)
-                outputs = self.decode(encoder_output ,encoder_states, root_batch, teacher_forcing_bool, epoch)
+                outputs = self.decode(encoder_states, root_batch, teacher_forcing_bool, epoch)
                 a = [torch.squeeze(item, 1) for item in outputs]
                 a = [torch.unsqueeze(item, 0) for item in a]
                 output = torch.cat(a, dim = 0)
@@ -227,19 +241,114 @@ class Model(nn.Module) :
 
         return epoch_loss / n
     
-    def predict(self, word):
-        '''
-        this is the adaptation of encoder-decoder network on a single word w/o optimization
-        '''
+    
+
+    def each_char(self,depth, acc_char, predicted_chars, predicted_proba, hidden, cell):
+
+        #print("the parms :", depth, result_list, acc_char, predicted_chars, predicted_proba)
+
+        acc1, acc2, acc3 = acc_char[:], acc_char[:], acc_char[:] 
+
+        test_acc_char = acc_char
+        #print("hh", test_acc_char)
+
+        char1, prob1 = predicted_chars[0],predicted_proba[0]
+        char2, prob2 = predicted_chars[1],predicted_proba[1]
+        char3, prob3 = predicted_chars[2],predicted_proba[2]
+
+        acc_char1,predicted_chars1, predicted_proba1, hidden1, cell1 = self.run_char_through_NN(acc1, char1, prob1, hidden, cell, depth)
+        acc_char2,predicted_chars2, predicted_proba2, hidden2, cell2 = self.run_char_through_NN(acc2,char2, prob2, hidden, cell, depth)
+        acc_char3,predicted_chars3, predicted_proba3, hidden3, cell3 = self.run_char_through_NN(acc3, char3, prob3, hidden, cell, depth)
+
+        #print(acc_char1, acc_char2, acc_char3)
+
+        test_acc_char = acc_char1
         
+        if len(test_acc_char) >= 5 :
+            #print(test_acc_char)
+            self.result_list.append([acc_char1,predicted_proba1])
+            self.result_list.append([acc_char2,predicted_proba2])
+            self.result_list.append([acc_char3,predicted_proba3])
+            self.counter += 3
+            #print('stopping cond incremented +3 :', self.counter)
+            d = depth ** 4
+            #print(self.counter, d)
+            if self.counter >= d:
+                return 
+
+
+        else : 
+
+            self.each_char(depth,acc_char1,predicted_chars1, predicted_proba1, hidden1, cell1)
+            self.each_char(depth,acc_char2,predicted_chars2, predicted_proba2, hidden2, cell2)
+            self.each_char(depth,acc_char3,predicted_chars3, predicted_proba3, hidden3, cell3)
+
+
+
+                
+
+    def run_char_through_NN(self,acc_char, char, proba, hidden, cell, depth): 
+        acc = acc_char
+        acc.append(char)
+        #print("haizueazube", acc, char)
+        if len(acc_char) == 5:
+            return acc,char, proba, hidden , cell
+        else :  
+            # input :  item , item , item , item 
+            embedded_char = torch.unsqueeze(self.embedding(torch.tensor(char)), 0)
+            # we create a softmax layer : 
+            soft = nn.Softmax(dim = 1)
+            
+            #print(hidden.size(), cell.size())
+            decoder_output , (final_hidden, final_cell) = self.LSTM(embedded_char, (hidden, cell))
+            input_decoder_output = self.input_dense(decoder_output)
+            embedded_char = input_decoder_output
+            Dense_decoded_output = self.Linear(decoder_output)
+            output_proba = soft(Dense_decoded_output)
+            best_proba, best_indices = torch.topk(output_proba, depth).values.tolist()[0], torch.topk(output_proba, depth).indices.tolist()[0]
+
+            predicted_chars = best_indices
+            predicted_proba = [proba*item for item in best_proba] # accumulated probability
+
+            # returns list , list, item , item
+            return acc,predicted_chars, predicted_proba, final_hidden, final_cell
+
+
+    
+    def beam_search_decoding(self, hidden, cell , depth):
+
+        init_char = self.char_index_dic[self.sow]
+
+        acc_char,predicted_chars, predicted_proba, final_hidden, final_cell = self.run_char_through_NN([], init_char, 1, hidden , cell, depth)
+        
+        #print("initial parms : ",acc_char,predicted_chars,predicted_proba)
+
+        self.each_char(depth,acc_char,predicted_chars, predicted_proba, final_hidden, final_cell)
+
+        res = self.result_list
+
+        sorted_list = sorted(res, key = lambda x : x[1], reverse=True) 
+
+        selected_char = [item[0][1:-1] for item in sorted_list[0:10]]
+
+        print('the most probable outputs using beam search decoding are :', selected_char)
+
+        return selected_char
+
+
+
+    def predict(self, word):
+        
+        test_word = self.sow + word + self.eow
         # Let's turn the word into a sequence of word indexes 
-        word_seq = self.word_to_seq(word)
+        word_seq = self.word_to_seq(test_word)
         # Let's create an embedding of the word seq
         embedded_word = self.embedding(torch.tensor(word_seq))
         init_hid = nn.init.xavier_normal_(torch.zeros(2*self.num_layers, self.hidden_size), gain=0.5)
         init_ce = nn.init.xavier_normal_(torch.zeros(2*self.num_layers, self.hidden_size), gain=0.5)
         # Let's feed our word embedding to the encoder network
         outputs, (hidden, cell) = self.BILSTM(embedded_word, (init_hid, init_ce))
+        
         #print(hidden.size())
         final_hid, final_ce = [], []
         for k in range(0,hidden.size(0), 2):
@@ -252,101 +361,105 @@ class Model(nn.Module) :
 
             final_hid.append(cct_hid)
             final_ce.append(cct_ce)
-        
+    
         final_hidden, final_cell = torch.tensor(final_hid), torch.tensor(final_ce)
-        #initialize the input of the decoder
-        embedded_char = torch.unsqueeze(self.embedding(torch.tensor(self.char_index_dic[self.sow])), 0)
-        prediction_output = [] # a list of the outputs of the decoder 
-        # we create a softmax layer : 
-        soft = nn.Softmax(dim = 1)
+
+        results = self.beam_search_decoding(final_hidden, final_cell, depth=3)
+
         key_list = list(self.char_index_dic.keys())
         val_list = list(self.char_index_dic.values())
-        for i in range(5):              
-            decoder_output , (final_hidden, final_cell) = self.LSTM(embedded_char, (final_hidden, final_cell))
-            input_dense = nn.Linear(self.hidden_size * 2,self.embedding_size)
-            input_decoder_output = input_dense(decoder_output)
-            embedded_char = input_decoder_output
-            Dense_decoded_output = self.Linear(decoder_output)
-            prediction_output.append(soft(Dense_decoded_output).tolist())
 
-        prediction_output = torch.squeeze(torch.tensor(prediction_output), 1)     
-        #print(prediction_output.size())
-        test_word_seq = word_seq[1:]
-        test_word_seq = test_word_seq[:-1]    
-        precision = 10     
-        top_idx = torch.topk(prediction_output, precision, dim = 1).indices
-     
-        #if self.char_index_dic[self.sow] in top_idx[0] : 
-        init_char = self.char_index_dic[self.sow]
-        '''else : 
-            init_char = top_idx[0][0]'''
-        
-        #if self.char_index_dic[self.eow] in top_idx[-1] : 
-        final_char = self.char_index_dic[self.eow]
-        '''else : 
-            final_char = top_idx[-1][0]'''     
-        grid = []
-        for i in range(precision): 
-            for j in range(precision):
-                for k in range(precision):
-                    tmp = []
-                    tmp.append((top_idx[1][i]).item())
-                    tmp.append((top_idx[2][j]).item())
-                    tmp.append((top_idx[3][k]).item())
-                    grid.append(tmp)
-        
-        # we check the possibilities :      
-        best_cases = []
-        for case in grid : 
-            s = [item for item in case if item in set(test_word_seq)] # we select elts from a that are in l 
-            b = [item for item in test_word_seq if item in set(s)] #          
-            if s == b and s != [] : 
-                best_cases.append(case)
-             
-        # potential roots :   
-        pot_seq = []
-        #print(test_word_seq)
-        for item in best_cases : 
-            #print(item)
-            tmp = [init_char] + item  + [final_char]
-            pot_seq.append(tmp)           
         final_roots =[]
-        for seq in pot_seq : 
+        for seq in results : 
             position = [val_list.index(item) for item in seq]
             result_char = [key_list[pos] for pos in position]
             predicted_root = ''.join(result_char)
             final_roots.append(predicted_root)
-        return final_roots
+
+        return final_roots     
+
+    def data_2_seq(self): 
+        '''
+        this function indexes our text data
+        '''
+        final_data = []
+        for instance in self.data : 
+            tmp = []
+            word = [self.char_index_dic[char] for char in instance[0]]
+            root = [self.char_index_dic[char] for char in instance[1]]
+            tmp.append(word)
+            tmp.append(root)
+            final_data.append(tmp)
+        return final_data
+        
+
+
+    def data_batches(self, data):
+        '''
+        this function creates our training batches
+        '''
+        size= self.batch_size 
+        batches = [data[i:i + size] for i in range(0, len(data), size)]
+        return batches
+    
+    def scale(self ,train_data , val_data):
+        '''
+        scale our data with standard scaler
+        '''
+        std_word, std_root =  StandardScaler(), StandardScaler()
+        
+        train_word, train_root,val_word, val_root = [],[], [], []
+        for instance in train_data: 
+            train_word.append(instance[0])
+            train_root.append(instance[1])
+        for instance in val_data : 
+            val_word.append(instance[0])
+            val_root.append(instance[1])
+        
+        std_word.fit_transform(train_word)
+        std_word.transform(val_word)
+        std_root.fit_transform(train_root)
+        std_root.transform(val_root)
+
+        train_data = [list(item) for item in zip(train_word, train_root)]
+        val_data = [list(item) for item in zip(val_word, val_root)]
+
+
+        return train_data, val_data
+
+    def prepare_batches(self):
+        data = self.data_2_seq()
+        data = random.sample(data, len(data))
+        data_size = len(data)
+        middle_index = int(data_size * self.ratio)
+        train_data, val_data = data[:middle_index], data[middle_index:]
+        #train_data , val_data = self.scale(train_data , val_data)
+        train_batches, val_batches = self.data_batches(train_data), self.data_batches(val_data)
+        print("the number of training batches: ", len(train_batches))
+        print("the number of validation batches: ", len(val_batches))
+        return train_batches, val_batches
+    
 
     def fit(self, num_epochs):
         """
         let's first prepare our data      
         """
         print(f'The model has {self.count_parameters():,} trainable parameters')
-        data = self.batches
-        data = random.sample(data, len(data))
-        data_size = len(data)
-        middle_index = int(data_size * self.ratio)        
-        train_batches , val_batches = data[:middle_index], data[middle_index:] 
+
         epochs = list(range(num_epochs))
         best_val_loss = 1000
         best_model_par = 0
         losses =[]
-        predicted_roots = []
         test_word = '$' + 'تحليل' + '£'
-  
         for epoch in epochs : 
-                
             print('epoch num : ', epoch) 
             t1 = time.time()
-            train_batches = random.sample(train_batches , len(train_batches))
+            train_batches = random.sample(self.train_batches , len(self.train_batches))
             #val_batches = random.sample(val_batches, len(val_batches))    
             train_loss= self.train_model(train_batches, 1, epoch)
-            val_loss = self.evaluate_model(val_batches, 0, epoch) # we set the teacher forcing to false            
+            val_loss = self.evaluate_model(self.val_batches, 0, epoch) # we set the teacher forcing to false            
             t2 = time.time()
-            predicted_root = self.predict(test_word)
-            print(predicted_root)
-            predicted_roots.append(predicted_root)
+            
             tmp = [train_loss, val_loss]
             losses.append(tmp)
             
@@ -356,7 +469,9 @@ class Model(nn.Module) :
             if val_loss < best_val_loss :
                 best_val_loss = val_loss 
                 best_model_par = self.state_dict()
-        torch.save(best_model_par, 'best_model.pt')       
+        torch.save(best_model_par, 'projet NLP/best_model2.pt')
+        predicted_root = self.predict(test_word)
+        print(predicted_root)       
         return losses
     
     def count_parameters(self):
